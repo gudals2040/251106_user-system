38ca0ba78725f1ab79f08e48af43669edab36c46
PARK HYEONG MIN <qd0420@gmail.com>
2025-11-06 15:03:16 +0900

    steop02

--- src/main/java/org/example/usersystem/service/UserAccountService.java
+++ src/main/java/org/example/usersystem/service/UserAccountService.java
@@ -1,4 +1,22 @@
 package org.example.usersystem.service;
 
+import org.example.usersystem.model.entity.UserAccount;
+import org.example.usersystem.model.repository.UserAccountRepository;
+
 public class UserAccountService {
+    private final UserAccountRepository accountRepository = new UserAccountRepository();
+
+    public void createUser(String username, String password) {
+        accountRepository.createUser(new UserAccount(username, password));
+    }
+
+    public boolean login(String username, String password) {
+        try {
+            UserAccount account = accountRepository.findByUsername(username);
+            return account.password().equals(password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
